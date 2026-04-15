9502f7bc323f45c47b99c9e47649a89de0a9db13
idko2004 <ivancito72011@gmail.com>
2026-04-15 18:27:10 -0300

    actualizar config de laptop

--- scripts/startup-laptop.sh
+++ scripts/startup-laptop.sh
@@ -8,19 +8,29 @@ dunst &
 gsettings set org.gnome.desktop.interface cursor-theme "Posy_Cursor_125_175"
 gsettings set org.gnome.desktop.interface font-name 'Inter Medium'
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-hyprctl setcursor Posy_Cursor_125_175 24
+hyprctl setcursor Posy_Cursor_125_175 32
 
 # ejecutar después de un rato
 sleep 1.5
 waybar &
-hypridle &
 
-sleep 0.5
+sleep 1
 wl-paste --watch cliphist store &
+
+sleep 0.2
 nm-applet &
+
+sleep 0.2
 blueman-applet &
+
+sleep 0.2
 flatpak run org.keepassxc.KeePassXC &
 
-sleep 0.5
+sleep 0.2
 batsignal -b -m +40 -w 18 -c 10 -d 5 -D "/usr/bin/systemctl hibernate"
-"/home/idko/.nvm/versions/node/v20.5.0/bin/node" "/home/idko/Documents/Projects/scripts-and-things/scripts/notifyUpdate.js"
+
+sleep 0.2
+"/home/idko/.nvm/versions/node/v23.6.0/bin/node" "/home/idko/Documents/Projects/scripts-and-things/scripts/notifyUpdate.js"
+
+sleep 0.2
+hypridle &
